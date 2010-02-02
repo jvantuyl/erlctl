6de0184a6d7c7a3a2cf43dffbe66312a150a77ba
Jayson Vantuyl <jvantuyl@scatterbit.com>
2010-02-02 12:33:08 -0800

    Added Dummy Functions for Development

--- lib/erlctl-0.1/src/erlctl.erl
+++ lib/erlctl-0.1/src/erlctl.erl
@@ -1,1 +1,11 @@
 -module (erlctl).
+-export([format/1,format/2,exit_with_code/1]).
+
+% FIXME: These are dummy functions!
+
+format(Disp) -> format(Disp,[]).
+format(Disp,Opts) ->
+  io:format(Disp,Opts).
+
+exit_with_code(N) ->
+  io:format("Exited with status ~p!~n",[N]).
