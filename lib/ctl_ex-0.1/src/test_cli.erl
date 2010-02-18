2fbf2849745de59869b0ad602d4e3425d2890268
Jayson Vantuyl <jvantuyl@scatterbit.com>
2010-02-17 22:38:31 -0800

    Updated Design to Make More Sense
    Old design had bad side effect that missing commands would cause a new VM to be
    started.  Oops.  Now starting must be explicitly requested.  Also, return
    values are used to make this all smoother.
    
    Well, that's the new design at least.  Updated design docs and example.  Next,
    updating the actual code.

--- lib/ctl_ex-0.1/src/test_cli.erl
+++ lib/ctl_ex-0.1/src/test_cli.erl
@@ -3,32 +3,32 @@
 
 %% @doc Prints out the installed version of the ctl_ex server.
 version(none,[]) ->
-  format("Version: ~p~n",[0.1]),
-  exit_with_code(0).
+  {ok,"Version: ~p~n",[0.1]}.
 
 %% @doc Start the ctl_ex application.
 start(running,_) ->
-  format("Already running.~n"),
-  exit_with_code(1);
-start(start,[]) ->
+  {error,1,"Already running.~n"};
+start(not_running,[]) ->
+  {start,"Starting.~n"};
+start(started,[]) ->
   ok = application:start(ctl_ex),
   erlctl:format("Started~n",[]),
-  exit_with_code(0).
+  ok.
 
 %% @doc Stop the ctl_ex application.
 stop(not_running,[]) ->
   format("Not running.~n"),
-  exit_with_code(0);
+  ok;
 stop(running,[]) ->
   application:stop(ctl_ex),
   format("Stopping.~n"),
   server_exit(),
-  exit_with_code(0).
+  ok.
 
 %% @doc List the users.
 list_users(not_running,_) ->
   format("Not running.~n"),
-  exit_with_code(1);
+  {error,1};
 list_users(running,[]) ->
   {ok,Users} = ctl_ex:list_users(),
   format("Users:~n"),
@@ -38,27 +38,27 @@ list_users(running,[]) ->
     end,
     Users
   ),
-  exit_with_code(0).
+  ok.
 
 %% @doc Add a user.
 add_user(not_running,_) ->
   format("Not running.~n"),
-  exit_with_code(1);
+  {error,1};
 add_user(running,[User]) ->
   ok = ctl_ex:add_user(User),
   format("User added~n"),
-  exit_with_code(0).
+  ok.
 
 %% @doc Delete a user.
 del_user(not_running,_) ->
   format("Not running.~n"),
-  exit_with_code(1);
+  {error,1};
 del_user(running,[User]) ->
   case ctl_ex:del_user(User) of
     ok ->
       format("User deleted~n"),
-      exit_with_code(0);
+      ok;
     no_such_user ->
       format("Error: No Such User!~n"),
-      exit_with_code(1)
+      {error,1}
   end.
