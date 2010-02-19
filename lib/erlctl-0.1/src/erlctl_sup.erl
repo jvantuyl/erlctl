855674321481bb64e812d3dd60a09c61cc7aeed4
Jayson Vantuyl <jvantuyl@scatterbit.com>
2010-02-19 06:19:41 -0800

    Fixed Bug Introduced In App Fix

--- lib/erlctl-0.1/src/erlctl_sup.erl
+++ lib/erlctl-0.1/src/erlctl_sup.erl
@@ -7,12 +7,12 @@ start_link() ->
   supervisor:start_link(?MODULE,[]).
 
 init(_Args) ->
-  Restart = {one_for_all,5,1}
+  Restart = {one_for_all,5,1},
   %Handler = {
   %  erlctl_handler,
   %  {gen_event, start_link, [ {local, erlctl_handler} ]},
   %  permanent, 5000, worker, dynamic
-  },
+  %},
   Children = [
   %  Handler
   ],
